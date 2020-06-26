50be9a0a4c9d99734ea2b6c3029dfaccc6fa33ac
Saurabh_R_Nayak <46881166+SaurabhRNayak@users.noreply.github.com>
2020-06-26 21:18:57 +0530

    invalid file

--- week1/code submit.py
+++
@@ -1,4 +0,0 @@
-import requests
-url='https://lf8q0kx152.execute-api.us-east-2.amazonaws.com/default/computeFitnessScore'
-x=requests.post(url,json={"qconfig":"3 1 6 4 0 7 5 2","userID":694547,"githubLink":""})
-print(x.text)
